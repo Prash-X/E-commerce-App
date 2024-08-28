638b0f7818aec924f65d393f3616fca5fed1b170
prashantGidnavar <gidnavarp@gmail.com>
2024-08-28 10:44:43 +0530

    Spring security

---
+++ UserService/src/main/java/com/prashant/UserService/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.prashant.UserService.repo;
+
+
+import com.prashant.UserService.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, Integer> {
+
+    Users findByUsername(String username);
+}
